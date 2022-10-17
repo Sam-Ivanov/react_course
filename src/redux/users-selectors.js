770cdc59a12a81ba0a01065ab5d2adbb2ccfd3e7
Aleksandr Ivanov <1300087@gmail.com>
2022-10-17 18:46:28 +0300

    +++

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -12,7 +12,7 @@ export const getTotalUsersCount = (state) => {
 
 export const getCurrentPage = (state) => {
    return state.usersPage.currentPage
-}
+} 
 
 export const getIsFetching = (state) => {
    return state.usersPage.isFetching
